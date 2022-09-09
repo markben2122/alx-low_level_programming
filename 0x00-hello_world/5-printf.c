d049d26c99deaaa2c3017cb6d77f7a733662cdcd
markben2122 <marknehemiah14@gmial.com>
2022-09-08 19:59:15 -0700

    new commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the printf function
+ *  this program prints "Programming is like building a multilingual"
+ *  Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
