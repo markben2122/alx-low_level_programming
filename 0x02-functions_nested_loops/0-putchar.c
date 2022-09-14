c76f4810ea9dcadb546bc946ef749581e235553a
markben2122 <marknehemiah14@gmial.com>
2022-09-13 17:39:28 -0700

    git push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
 /**
- * main -  prints Holberton, followed by a new line
+ * main - Prints _putchar as a message.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
-}
