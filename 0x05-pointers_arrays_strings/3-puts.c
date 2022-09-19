0c0d2917d00542e871b31a6d5410374ec9361fae
markben2122 <marknehemiah14@gmial.com>
2022-09-19 15:26:39 -0700

    my first commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
-#include "stdio.h"
 
 /**
- * _puts - prints a string
- * @str: The string to be printed.
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
-
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*str)
-		_putchar(*str++);
-
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
