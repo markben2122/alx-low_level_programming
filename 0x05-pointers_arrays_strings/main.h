8175b8267227446930ba62f8d2bf8a401074ea83
markben2122 <marknehemiah14@gmial.com>
2022-09-19 20:38:28 -0700

    the newest commit -m

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,8 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "ctype.h"
-#include "string.h"
-#include "string.h"
+#ifndef MAIN_H
+#define MAIN_H
 
+int _atoi(char *s);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,4 +13,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+
+#endif
