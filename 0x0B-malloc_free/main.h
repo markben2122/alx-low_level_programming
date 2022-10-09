d63b6d62a5e1dfcef2e59808ab04ab7a4a8e75ca
mark nehemiah <105027765+markben2122@users.noreply.github.com>
2022-10-09 17:01:19 +0100

    new commit

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, char *s2);
+char*_strdup(char *str);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
+char **strow(char *str);
+
+#endif /*MAIN_H */
